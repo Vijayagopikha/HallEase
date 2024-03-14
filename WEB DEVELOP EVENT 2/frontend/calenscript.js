d99f3b6a4496377fb6530610561587a86a9aea92
Vanjula <vanjulasankar@gmail.com>
2024-03-14 18:23:19 +0530

    calendar

--- WEB DEVELOP EVENT 2/frontend/calenscript.js
+++ WEB DEVELOP EVENT 2/frontend/calenscript.js
@@ -1,4 +1,7 @@
 const currentDate=document.querySelector(".current-date");
+daysTag=document.querySelector(".days");
+prevNextIcon=document.querySelectorAll(".icons i");
+
 
 let date=new Date(),
 currYear=date.getFullYear(),
@@ -8,7 +11,23 @@ const months =["January","February","March","April","May","June","July","August"
 
 const renderCalendar = () => {
     let lastDateOfMonth=new Date(currYear,currMonth+1,0).getDate();
+    let liTag="";
+    for(let i=0;i<=lastDateOfMonth;i++)
+    {
+        liTag+=`<li>${i}</li>`;
+    }
  currentDate.innerText= `${months[currMonth]} ${currYear}` ;
+ daysTag.innerHTML=liTag;
 }
+
 renderCalendar();
+
+prevNextIcon.foreach(icon =>{
+    icon.addEventListener("click",()=>{
+        console.log(icon);
+    });
+});
+
+
+
 
