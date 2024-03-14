88898a29aafbc718596f62ac9374a1ed91057404
Vijayagopikha <115525092+Vijayagopikha@users.noreply.github.com>
2024-03-14 07:35:40 +0530

    bjj

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import core from 'cors';
 
 
@@ -22,7 +23,52 @@ app.use(core());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true},
+  password: String,
+});
 
+userSchema.pre('save',async function(next){
+  if(!this.isModified('password')){
+  return next();
+}
+try{
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.passworrd, salt);
+  next();
+}catch(error){
+  next(error);
+}
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/login', async(req, res)=>{
+  const { email, password} = req.body;
+  try{
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(401).json({ message: 'Invalid email or passowrd'});
+
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+      return res.status(401).json({message: 'Invalid email or password'});
+
+    }
+
+    const jwtSecretKey = require('crypto').randomBytes(64).toString('hex');
+    const token = jwt.sign({email: user.email}, jwtSecretKey);  
+  
+    res.status(200).json({token});
+  }
+  catch(error){
+    console.error('Login error:', error);
+    res.status(500).json({message: 'Internal server error'});
+
+  }
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
