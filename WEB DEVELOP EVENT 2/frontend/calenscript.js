fe2546d406b064369a75451f20bc0126fffad112
Vanjula <vanjulasankar@gmail.com>
2024-03-14 22:30:19 +0530

    eer

--- WEB DEVELOP EVENT 2/frontend/calenscript.js
+++ WEB DEVELOP EVENT 2/frontend/calenscript.js
@@ -9,7 +9,7 @@ let date = new Date(),
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const renderCalendar = () => {
-<<<<<<< HEAD
+
     let firstDayOfMonth=new Date(currYear,currMonth,1).getDay();
     lastDateOfMonth=new Date(currYear,currMonth+1,0).getDate();
     lastDayOfMonth=new Date(currYear,currMonth,lastDateOfMonth).getDay();
@@ -35,24 +35,8 @@ const renderCalendar = () => {
 
  currentDate.innerText= `${months[currMonth]} ${currYear}` ;
  daysTag.innerHTML=liTag;
-=======
-    let firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
-    lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
-    lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
+ attachDateClickEvent(); // Attach click event to each date
 
-    let liTag = "";
-
-    for (let i = firstDayOfMonth; i > 0; i--) {
-        liTag += `<li>${lastDateOfLastMonth - i + 1}</li>`;
-    }
-
-    for (let i = 1; i <= lastDateOfMonth; i++) {
-        liTag += `<li class="date">${i}</li>`;
-    }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`;
-    daysTag.innerHTML = liTag;
-    attachDateClickEvent(); // Attach click event to each date
->>>>>>> 2f75af930ca40f036afcdf8266d5f264432c614e
 }
 
 renderCalendar();
